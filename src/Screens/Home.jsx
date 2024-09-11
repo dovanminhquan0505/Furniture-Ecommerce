72e7504d2c20f9630cde820fd6a6f58ebfe27398
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-11 16:06:01 +0700

    create Home screen

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,8 +1,44 @@
 import React from "react";
 import Helmet from "../components/Helmet/Helmet";
+import heroImg from '../assets/images/hero-img.png'
+import { Container, Row, Col } from "reactstrap";
 
 const Home = () => {
-    return <Helmet title={" Home"}></Helmet>;
+    const year = new Date().getFullYear();
+    return (
+        <Helmet title={" Home"}>
+            <section className="hero__section">
+                <Container>
+                    <Row>
+                        <Col lg="6" md="6">
+                            <div className="hero__content">
+                                <p className="hero_subtitle">
+                                    Trending Product In {year}
+                                </p>
+                                <h2>
+                                    Make Your Interior More Minimalistic &
+                                    Modern
+                                </h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Nam, cum! Voluptatum
+                                    praesentium quidem facilis maiores rerum
+                                    animi necessitatibus? Corrupti, consectetur?
+                                </p>
+                                <button className="buy__btn">SHOP NOW</button>
+                            </div>
+                        </Col>
+
+                        <Col lg="6" md="6">
+                            <div className="hero__img">
+                                <img src={heroImg} alt="" />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </Helmet>
+    );
 };
 
 export default Home;

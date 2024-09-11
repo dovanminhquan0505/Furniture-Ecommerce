583fcca878a9d88d4cdf880306498d0024118ecd
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-11 10:43:53 +0700

    create form of layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Routers from "../../routers/Routers";
 
 const Layout = () => {
-    return <div>Layout</div>;
+    return (
+        <>
+            <Header />
+            <div>
+                <Routers />
+            </div>
+            <Footer />
+        </>
+    );
 };
 
 export default Layout;

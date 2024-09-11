814bd14b91f85c6f194c0fd53dbd539129716075
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-11 10:32:50 +0700

    create Routers

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,7 +1,27 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+//Import Screens
+import Home from "../Screens/Home";
+import Shop from "../Screens/Shop";
+import Cart from "../Screens/Cart";
+import ProductDetails from "../Screens/ProductDetails";
+import Checkout from "../Screens/Checkout";
+import Login from "../Screens/Login";
+import SignUp from "../Screens/SignUp";
 
 const Routers = () => {
-    return <div>Routers</div>;
+    //Link URL to views for each router
+    return (
+        <Routes>
+            <Route path="home" element={<Home />} />
+            <Route path="shop" element={<Shop />} />
+            <Route path="shop/:id" element={<ProductDetails />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<SignUp />} />
+        </Routes>
+    );
 };
 
 export default Routers;

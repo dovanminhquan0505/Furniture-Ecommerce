f475907b63889c12e23ef38669b63a35f411a113
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-12 17:23:32 +0700

    add sticky header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "./header.css";
 import { motion } from "framer-motion";
@@ -23,8 +23,30 @@ const nav__links = [
 ];
 
 const Header = () => {
+    const headerRef = useRef(null);
+
+    const stickyHeaderFunc = () => {
+        window.addEventListener("scroll", () => {
+            if (
+                document.body.scrollTop > 80 ||
+                document.documentElement.scrollTop > 80
+            ) {
+                headerRef.current.classList.add("sticky__header");
+            } else {
+                headerRef.current.classList.remove("sticky__header");
+            }
+        });
+    };
+
+    useEffect(() => {
+        stickyHeaderFunc();
+        return () => {
+            window.removeEventListener("scroll", stickyHeaderFunc);
+        };
+    });
+
     return (
-        <header className="header">
+        <header className="header" ref={headerRef}>
             <Container>
                 <Row>
                     <div className="nav__wrapper">

4064b63df5ef0abd92d918aacafcc536b6d1da00
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-12 14:45:04 +0700

    add items data for productCard

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -1,29 +1,28 @@
 import React from "react";
-import productImage from "../../assets/images/arm-chair-01.jpg";
 import "../../styles/product-card.css";
 import { motion } from "framer-motion";
 import { Col } from "reactstrap";
 import { Link } from "react-router-dom";
 
-const ProductCard = () => {
+const ProductCard = ({ item }) => {
     return (
         <Col lg="3" md="4">
             <div className="product__item">
                 <div className="product__img">
                     <motion.img
                         whileHover={{ scale: 0.9 }}
-                        src={productImage}
+                        src={item.imgUrl}
                         alt=""
                     />
                 </div>
                 <div className="p-2 product__info">
                     <h3 className="product__name">
-                        <Link to="/shop/id">Modern Arm Chair</Link>
+                        <Link to={`/shop/${item.id}`}>{item.productName}</Link>
                     </h3>
-                    <span>Chair</span>
+                    <span>{item.category}</span>
                 </div>
                 <div className="product__card-bottom d-flex align-items-center justify-content-between p-2">
-                    <span className="price">$299</span>
+                    <span className="price">${item.price}</span>
                     <motion.span whileTap={{ scale: 1.2 }}>
                         <i class="ri-add-line"></i>
                     </motion.span>

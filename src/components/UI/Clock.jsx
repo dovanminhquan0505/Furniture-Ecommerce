c472e00af3692592b043a530d62b443fc34bfbf9
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-12 15:37:45 +0700

    create form of clock counter time

--- src/components/UI/Clock.jsx
+++ src/components/UI/Clock.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Clock = () => {
-    return <div>Clock</div>;
+    return <div className="clock__wrapper d-flex align-items-center gap-3">
+        <div className="clock__data d-flex align-items-center gap-3">
+            <div className="text-center">
+                <h1 className="text-white fs-3 mb-2">10 </h1>
+                <h5 className="text-white fs-6">Days</h5>
+            </div>
+            <span className="text-white fs-3">:</span>
+        </div>
+
+        <div className="clock__data d-flex align-items-center gap-3">
+            <div className="text-center">
+                <h1 className="text-white fs-3 mb-2">10 </h1>
+                <h5 className="text-white fs-6">Hours</h5>
+            </div>
+            <span className="text-white fs-3">:</span>
+        </div>
+
+        <div className="clock__data d-flex align-items-center gap-3">
+            <div className="text-center">
+                <h1 className="text-white fs-3 mb-2">10 </h1>
+                <h5 className="text-white fs-6">Minutes</h5>
+            </div>
+            <span className="text-white fs-3">:</span>
+        </div>
+
+        <div className="clock__data d-flex align-items-center gap-3">
+            <div className="text-center">
+                <h1 className="text-white fs-3 mb-2">10 </h1>
+                <h5 className="text-white fs-6">Seconds</h5>
+            </div>
+        </div>
+    </div>
 };
 
 export default Clock;

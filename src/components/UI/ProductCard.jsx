9b88a0f1c7813d4365031877a333602f421f064c
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-12 14:21:16 +0700

    add some animation for img, icon and add some style css for product-card

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -3,21 +3,30 @@ import productImage from "../../assets/images/arm-chair-01.jpg";
 import "../../styles/product-card.css";
 import { motion } from "framer-motion";
 import { Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const ProductCard = () => {
     return (
         <Col lg="3" md="4">
             <div className="product__item">
                 <div className="product__img">
-                    <img src={productImage} alt="" />
+                    <motion.img
+                        whileHover={{ scale: 0.9 }}
+                        src={productImage}
+                        alt=""
+                    />
                 </div>
-                <h3 className="product__name">Modern Arm Chair</h3>
-                <span>Chair</span>
-                <div className="product__card-bottom">
+                <div className="p-2 product__info">
+                    <h3 className="product__name">
+                        <Link to="/shop/id">Modern Arm Chair</Link>
+                    </h3>
+                    <span>Chair</span>
+                </div>
+                <div className="product__card-bottom d-flex align-items-center justify-content-between p-2">
                     <span className="price">$299</span>
-                    <span>
+                    <motion.span whileTap={{ scale: 1.2 }}>
                         <i class="ri-add-line"></i>
-                    </span>
+                    </motion.span>
                 </div>
             </div>
         </Col>

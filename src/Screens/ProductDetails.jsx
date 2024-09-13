41d6a62b9ff976675068ad64787069d10d2357f4
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-13 22:15:48 +0700

    create tab for description and reviews

--- src/Screens/ProductDetails.jsx
+++ src/Screens/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "reactstrap";
 import { useParams } from "react-router-dom";
 import products from "../assets/data/products";
@@ -8,6 +8,7 @@ import "../styles/product-details.css";
 import { motion } from "framer-motion";
 
 const ProductDetails = () => {
+    const [tab, setTab] = useState("desc");
     const { id } = useParams();
     const product = products.find((item) => item.id === id);
 
@@ -62,7 +63,10 @@ const ProductDetails = () => {
                                 <span className="product__price">${price}</span>
                                 <p className="mt-3">{shortDesc}</p>
 
-                                <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
+                                <motion.button
+                                    whileTap={{ scale: 1.2 }}
+                                    className="buy__btn"
+                                >
                                     Add to Cart
                                 </motion.button>
                             </div>
@@ -76,13 +80,31 @@ const ProductDetails = () => {
                     <Row>
                         <Col lg="12">
                             <div className="tab__wrapper d-flex align-items-center gap-5">
-                                <h6>Description</h6>
-                                <h6>Reviews ({reviews.length})</h6>
+                                <h6
+                                    className={`${
+                                        tab === "desc" ? "active__tab" : ""
+                                    }`}
+                                    onClick={() => setTab("desc")}
+                                >
+                                    Description
+                                </h6>
+                                <h6
+                                    className={`${
+                                        tab === "rev" ? "active__tab" : ""
+                                    }`}
+                                    onClick={() => setTab("rev")}
+                                >
+                                    Reviews ({reviews.length})
+                                </h6>
                             </div>
 
-                            <div className="tab__content">
-                                <p>{description}</p>
-                            </div>
+                            {tab === "desc" ? (
+                                <div className="tab__content mt-4">
+                                    <p>{description}</p>
+                                </div>
+                            ) : (
+                                <div>reviews</div>
+                            )}
                         </Col>
                     </Row>
                 </Container>

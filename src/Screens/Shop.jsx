2e4e20f08a79190ab1e2529e20ecbdbb1ae6a866
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-13 10:34:55 +0700

    fix css for commonSection for Shop

--- src/Screens/Shop.jsx
+++ src/Screens/Shop.jsx
@@ -3,7 +3,11 @@ import CommonSection from "../components/UI/CommonSection";
 import Helmet from "../components/Helmet/Helmet";
 
 const Shop = () => {
-    return <div>Shop</div>;
+    return (
+        <Helmet title=" Shop">
+            <CommonSection title="Products" />
+        </Helmet>
+    );
 };
 
 export default Shop;

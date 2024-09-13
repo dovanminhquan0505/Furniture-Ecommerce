500c80a68a5b8e4ee3064b588c16fae072d65327
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-13 19:35:49 +0700

    create form product details

--- src/Screens/ProductDetails.jsx
+++ src/Screens/ProductDetails.jsx
@@ -1,7 +1,74 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { useParams } from "react-router-dom";
+import products from "../assets/data/products";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+import "../styles/product-details.css";
 
 const ProductDetails = () => {
-    return <div>ProductDetails</div>;
+    const { id } = useParams();
+    const product = products.find((item) => item.id === id);
+
+    const {
+        imgUrl,
+        productName,
+        price,
+        avgRating,
+        review,
+        description,
+        shortDesc,
+    } = product;
+
+    return (
+        <Helmet title="">
+            <CommonSection />
+
+            <section className="pt-0">
+                <Container>
+                    <Row>
+                        <Col lg="6">
+                            <img src={imgUrl} alt="" />
+                        </Col>
+
+                        <Col lg="6">
+                            <div className="product__details">
+                                <h1>{productName}</h1>
+                                <div className="product__rating">
+                                    <div>
+                                        <span>
+                                            <i class="ri-star-s-fill"></i>
+                                        </span>
+                                        <span>
+                                            <i class="ri-star-s-fill"></i>
+                                        </span>
+                                        <span>
+                                            <i class="ri-star-s-fill"></i>
+                                        </span>
+                                        <span>
+                                            <i class="ri-star-s-fill"></i>
+                                        </span>
+                                        <span>
+                                            <i class="ri-star-half-s-line"></i>
+                                        </span>
+                                    </div>
+
+                                    <p>({avgRating})</p>
+                                </div>
+
+                                <span>${price}</span>
+                                <p>{shortDesc}</p>
+
+                                <button className="buy__btn">
+                                    Add to Cart
+                                </button>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </Helmet>
+    );
 };
 
 export default ProductDetails;

ae6349c424a6b2dda2854538728c411d422f85b2
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-13 08:53:44 +0700

    use App into Provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
+// Use App into Provider, so that all child components inside App will have access to Redux store via connect or useSelector,
+// useDispatch hooks of react-redux.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <Provider store={store}>
+                <App />
+            </Provider>
         </BrowserRouter>
     </React.StrictMode>
 );

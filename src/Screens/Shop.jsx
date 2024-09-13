bc23b67fff7cfc8fc280c7fb57986ebd37ebf886
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-13 11:09:11 +0700

    show products into shop screen

--- src/Screens/Shop.jsx
+++ src/Screens/Shop.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import CommonSection from "../components/UI/CommonSection";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col } from "reactstrap";
 import "../styles/shop.css";
+import products from "../assets/data/products";
+import ProductsList from "../components/UI/ProductsList";
 
 const Shop = () => {
+    const [productsData, setProductsData] = useState(products);
+
+    const handleFilter = (e) => {
+        const filterValue = e.target.value;
+        if (filterValue === "sofa") {
+            const filteredProducts = products.filter(
+                (item) => item.category === "sofa"
+            );
+
+            setProductsData(filteredProducts);
+        }
+    };
+
     return (
         <Helmet title=" Shop">
             <CommonSection title="Products" />
@@ -14,7 +29,7 @@ const Shop = () => {
                     <Row>
                         <Col lg="3" md="3">
                             <div className="filter__widget">
-                                <select>
+                                <select onChange={handleFilter}>
                                     <option>Filter By Category</option>
                                     <option value="sofa">Sofa</option>
                                     <option value="mobile">Mobile</option>
@@ -43,6 +58,18 @@ const Shop = () => {
                                 </span>
                             </div>
                         </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            <section>
+                <Container>
+                    <Row>
+                        {productsData.length === 0 ? (
+                            <h1>Products are not found!</h1>
+                        ) : (
+                            <ProductsList data={productsData} />
+                        )}
                     </Row>
                 </Container>
             </section>

9f7c0f38e66f19f008121f16162d0c7e782f5701
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-15 21:22:54 +0700

    create checkout_cart form

--- src/Screens/Checkout.jsx
+++ src/Screens/Checkout.jsx
@@ -6,50 +6,92 @@ import CommonSection from "../components/UI/CommonSection";
 import "../styles/checkout.css";
 
 const Checkout = () => {
-    return <Helmet title=" Checkout">
-        <CommonSection title="Checkout" />
+    return (
+        <Helmet title=" Checkout">
+            <CommonSection title="Checkout" />
 
-        <section>
-            <Container>
-                <Row>
-                    <Col lg="8">
-                        <h6 className="mb-4 fw-bold">Billing Information</h6>
-                        <Form>
-                            <FormGroup className="form__group">
-                                <input type="text" placeholder="Enter your name" />
-                            </FormGroup>
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg="8">
+                            <h6 className="mb-4 fw-bold">
+                                Billing Information
+                            </h6>
+                            <Form className="billing__form">
+                                <FormGroup className="form__group">
+                                    <input
+                                        type="text"
+                                        placeholder="Enter your name"
+                                    />
+                                </FormGroup>
 
-                            <FormGroup className="form__group">
-                                <input type="email" placeholder="example@gmail.com" />
-                            </FormGroup>
+                                <FormGroup className="form__group">
+                                    <input
+                                        type="email"
+                                        placeholder="example@gmail.com"
+                                    />
+                                </FormGroup>
 
-                            <FormGroup className="form__group">
-                                <input type="number" placeholder="Phone number" />
-                            </FormGroup>
+                                <FormGroup className="form__group">
+                                    <input
+                                        type="number"
+                                        placeholder="Phone number"
+                                    />
+                                </FormGroup>
 
-                            <FormGroup className="form__group">
-                                <input type="text" placeholder="Street address" />
-                            </FormGroup>
+                                <FormGroup className="form__group">
+                                    <input
+                                        type="text"
+                                        placeholder="Street address"
+                                    />
+                                </FormGroup>
 
-                            <FormGroup className="form__group">
-                                <input type="text" placeholder="City" />
-                            </FormGroup>
+                                <FormGroup className="form__group">
+                                    <input type="text" placeholder="City" />
+                                </FormGroup>
 
-                            <FormGroup className="form__group">
-                                <input type="text" placeholder="Postal code" />
-                            </FormGroup>
+                                <FormGroup className="form__group">
+                                    <input
+                                        type="text"
+                                        placeholder="Postal code"
+                                    />
+                                </FormGroup>
 
-                            <FormGroup className="form__group">
-                                <input type="text" placeholder="Country" />
-                            </FormGroup>
-                        </Form>
-                    </Col>
+                                <FormGroup className="form__group">
+                                    <input type="text" placeholder="Country" />
+                                </FormGroup>
+                            </Form>
+                        </Col>
 
-                    <Col lg="4"></Col>
-                </Row>
-            </Container>
-        </section>
-    </Helmet>
+                        <Col lg="4">
+                            <div className="checkout__cart">
+                                <h6>
+                                    Total Qty: <span>0</span>
+                                </h6>
+                                <h6>
+                                    Subtotal: <span>$120</span>
+                                </h6>
+                                <h6>
+                                    Shipping: <span>$10</span>
+                                </h6>
+                                <h6>Free Shipping</h6>
+                                <h4>
+                                    Total Cost: <span>$130</span>
+                                </h4>
+                            </div>
+
+                            <motion.button
+                                whileTap={{ scale: 1.2 }}
+                                className="buy__btn auth__btn w-100"
+                            >
+                                Place order
+                            </motion.button>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </Helmet>
+    );
 };
 
 export default Checkout;

e1902298b2be634200ec77f321096bac7bb13c17
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-16 13:56:53 +0700

    create login form

--- src/Screens/Login.jsx
+++ src/Screens/Login.jsx
@@ -9,7 +9,23 @@ const Login = () => {
         <section>
             <Container>
                 <Row>
-                    <Col lg="6"></Col>
+                    <Col lg="6" className="m-auto text-center">
+                        <h3 className="fw-bold fs-4">Login</h3>
+
+                        <Form className="auth__form">
+                            <FormGroup className="form__group">
+                                <input type="email" placeholder="Enter your email" />
+                            </FormGroup>
+
+                            <FormGroup className="form__group">
+                                <input type="password" placeholder="Enter your password" />
+                            </FormGroup>
+
+                            <button className="buy__btn auth__btn">Login</button>
+
+                            <p>Don't have any accounts? <Link to="/signup">Create an account</Link></p>
+                        </Form>
+                    </Col>
                 </Row>
             </Container>
         </section>

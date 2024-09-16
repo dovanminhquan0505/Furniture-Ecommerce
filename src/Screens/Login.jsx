73819fb79eba6a4590842065c50a11c978887198
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-16 14:07:04 +0700

    complete signin form and update css for login form

--- src/Screens/Login.jsx
+++ src/Screens/Login.jsx
@@ -1,35 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/login.css";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Login = () => {
-    return <Helmet title=" Login">
-        <section>
-            <Container>
-                <Row>
-                    <Col lg="6" className="m-auto text-center">
-                        <h3 className="fw-bold fs-4">Login</h3>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-                        <Form className="auth__form">
-                            <FormGroup className="form__group">
-                                <input type="email" placeholder="Enter your email" />
-                            </FormGroup>
+    return (
+        <Helmet title=" Login">
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg="6" className="m-auto text-center">
+                            <h3 className="fw-bold mb-4">Login</h3>
 
-                            <FormGroup className="form__group">
-                                <input type="password" placeholder="Enter your password" />
-                            </FormGroup>
+                            <Form className="auth__form">
+                                <FormGroup className="form__group">
+                                    <input
+                                        type="email"
+                                        placeholder="Enter your email"
+                                        value={email}
+                                        onChange={(e) =>
+                                            setEmail(e.target.value)
+                                        }
+                                    />
+                                </FormGroup>
 
-                            <button className="buy__btn auth__btn">Login</button>
+                                <FormGroup className="form__group">
+                                    <input
+                                        type="password"
+                                        placeholder="Enter your password"
+                                        value={password}
+                                        onChange={(e) =>
+                                            setPassword(e.target.value)
+                                        }
+                                    />
+                                </FormGroup>
 
-                            <p>Don't have any accounts? <Link to="/signup">Create an account</Link></p>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    </Helmet>
+                                <motion.button
+                                    whileTap={{ scale: 1.2 }}
+                                    type="submit"
+                                    className="buy__btn auth__btn mb-3"
+                                >
+                                    Login
+                                </motion.button>
+
+                                <p>
+                                    Don't have any accounts?{" "}
+                                    <Link to="/signup">Create an account</Link>
+                                </p>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </Helmet>
+    );
 };
 
 export default Login;

83440fe307645e1e62191c29eaa84bfd2ccb1e90
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-16 14:28:28 +0700

    create register form and update css for it

--- src/Screens/SignUp.jsx
+++ src/Screens/SignUp.jsx
@@ -1,7 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/signup.css";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
-const SignUp = () => {
-    return <div>SignUp</div>;
+const Signup = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [file, setFile] = useState(null);
+
+    return (
+        <Helmet title=" Register">
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg="6" className="m-auto text-center">
+                            <h3 className="fw-bold mb-4">Register</h3>
+
+                            <Form className="auth__form">
+                                <FormGroup className="form__group">
+                                    <input
+                                        type="text"
+                                        placeholder="Username"
+                                        value={username}
+                                        onChange={(e) =>
+                                            setUsername(e.target.value)
+                                        }
+                                    />
+                                </FormGroup>
+                                <FormGroup className="form__group">
+                                    <input
+                                        type="email"
+                                        placeholder="Enter your email"
+                                        value={email}
+                                        onChange={(e) =>
+                                            setEmail(e.target.value)
+                                        }
+                                    />
+                                </FormGroup>
+
+                                <FormGroup className="form__group">
+                                    <input
+                                        type="password"
+                                        placeholder="Enter your password"
+                                        value={password}
+                                        onChange={(e) =>
+                                            setPassword(e.target.value)
+                                        }
+                                    />
+                                </FormGroup>
+
+                                <FormGroup className="form__group">
+                                    <input
+                                        type="file"
+                                        onChange={(e) =>
+                                            setFile(e.target.files[0])
+                                        }
+                                    />
+                                </FormGroup>
+
+                                <motion.button
+                                    whileTap={{ scale: 1.2 }}
+                                    type="submit"
+                                    className="buy__btn auth__btn mb-3"
+                                >
+                                    Sign up
+                                </motion.button>
+
+                                <p>
+                                    Already have any accounts?{" "}
+                                    <Link to="/login">Login</Link>
+                                </p>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </Helmet>
+    );
 };
 
-export default SignUp;
+export default Signup;

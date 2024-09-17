719b37b2092c8a6cc2fdaa8903f16ea2bf10191a
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-17 13:20:35 +0700

    Add useAuth and ProtectedRoute

---
+++ src/routers/ProtectedRoute.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import useAuth from "../custom-hooks/useAuth";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+    const { currentUser } = useAuth();
+    const [loading, setLoading] = useState(false);
+
+    if (loading)
+        return (
+            <div className="text-center">
+                <h5 className="fw-bold">Loading...</h5>
+            </div>
+        );
+
+    return currentUser ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;

91c3f62b518f7df58056e25e216c47713d9ad1d0
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-17 12:38:24 +0700

    complete Log in to Checkout

--- src/Screens/Login.jsx
+++ src/Screens/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../styles/login.css";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase.config";
@@ -12,57 +12,84 @@ const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     const signIn = async (e) => {
         e.preventDefault();
         setLoading(true);
-    }
+
+        try {
+            const userCredential = await signInWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+
+            const user = userCredential.user;
+
+            console.log(user);
+            setLoading(false);
+            toast.success("Successfully logged in!");
+            navigate("/checkout");
+        } catch (error) {
+            setLoading(false);
+            toast.error(error.message);
+        }
+    };
 
     return (
         <Helmet title=" Login">
             <section>
                 <Container>
                     <Row>
-                        <Col lg="6" className="m-auto text-center">
-                            <h3 className="fw-bold mb-4">Login</h3>
+                        {loading ? (
+                            <Col lg="12" className="text-center">
+                                <h5 className="fw-bold">Loading...</h5>
+                            </Col>
+                        ) : (
+                            <Col lg="6" className="m-auto text-center">
+                                <h3 className="fw-bold mb-4">Login</h3>
 
-                            <Form className="auth__form">
-                                <FormGroup className="form__group">
-                                    <input
-                                        type="email"
-                                        placeholder="Enter your email"
-                                        value={email}
-                                        onChange={(e) =>
-                                            setEmail(e.target.value)
-                                        }
-                                    />
-                                </FormGroup>
+                                <Form className="auth__form" onSubmit={signIn}>
+                                    <FormGroup className="form__group">
+                                        <input
+                                            type="email"
+                                            placeholder="Enter your email"
+                                            value={email}
+                                            onChange={(e) =>
+                                                setEmail(e.target.value)
+                                            }
+                                        />
+                                    </FormGroup>
 
-                                <FormGroup className="form__group">
-                                    <input
-                                        type="password"
-                                        placeholder="Enter your password"
-                                        value={password}
-                                        onChange={(e) =>
-                                            setPassword(e.target.value)
-                                        }
-                                    />
-                                </FormGroup>
+                                    <FormGroup className="form__group">
+                                        <input
+                                            type="password"
+                                            placeholder="Enter your password"
+                                            value={password}
+                                            onChange={(e) =>
+                                                setPassword(e.target.value)
+                                            }
+                                        />
+                                    </FormGroup>
 
-                                <motion.button
-                                    whileTap={{ scale: 1.2 }}
-                                    type="submit"
-                                    className="buy__btn auth__btn mb-3"
-                                >
-                                    Login
-                                </motion.button>
+                                    <motion.button
+                                        whileTap={{ scale: 1.2 }}
+                                        type="submit"
+                                        className="buy__btn auth__btn mb-3"
+                                    >
+                                        Login
+                                    </motion.button>
 
-                                <p>
-                                    Don't have any accounts?{" "}
-                                    <Link to="/signup">Create an account</Link>
-                                </p>
-                            </Form>
-                        </Col>
+                                    <p>
+                                        Don't have any accounts?{" "}
+                                        <Link to="/signup">
+                                            Create an account
+                                        </Link>
+                                    </p>
+                                </Form>
+                            </Col>
+                        )}
                     </Row>
                 </Container>
             </section>

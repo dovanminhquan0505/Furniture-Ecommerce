d020d60ee8da8ef8a6ec3a5d38f55b7b16d922a8
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-17 17:32:53 +0700

    Update Layout and create AdminNav

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -6,9 +6,15 @@ import AdminNav from "../../admin/AdminNav";
 import { useLocation } from "react-router-dom";
 
 const Layout = () => {
+    const location = useLocation();
+
     return (
         <>
-            <Header />
+            {location.pathname.startsWith("/dashboard") ? (
+                <AdminNav />
+            ) : (
+                <Header />
+            )}
             <div>
                 <Routers />
             </div>

92e8b86a200260bc223abdcf36c3b66cf40ded01
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-18 19:55:01 +0700

    Update AdminNav form and css

--- src/admin/AdminNav.jsx
+++ src/admin/AdminNav.jsx
@@ -1,34 +1,94 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row } from "reactstrap";
 import useAuth from "../custom-hooks/useAuth";
 import "../styles/admin-nav.css";
+import { NavLink } from "react-router-dom";
+
+const admin_nav = [
+    {
+        display: "Dashboard",
+        path: "/dashboard",
+    },
+
+    {
+        display: "All-Products",
+        path: "/dashboard/all-products",
+    },
+
+    {
+        display: "Orders",
+        path: "/dashboard/orders",
+    },
+
+    {
+        display: "Users",
+        path: "/dashboard/users",
+    },
+];
 
 const AdminNav = () => {
-    const {currentUser} = useAuth();
+    const { currentUser } = useAuth();
 
     return (
-        <header className="admin__header">
-            <div className="admin__nav-top">
+        <>
+            <header className="admin__header">
+                <div className="admin__nav-top">
+                    <Container>
+                        <div className="admin__nav-wrapper-top">
+                            <div className="logo">
+                                <h2>Multimart</h2>
+                            </div>
+
+                            <div className="search__box">
+                                <input type="text" placeholder="Search..." />
+                                <span>
+                                    <i class="ri-search-line"></i>
+                                </span>
+                            </div>
+
+                            <div className="admin__nav-top-right">
+                                <span>
+                                    <i class="ri-notification-2-line"></i>
+                                </span>
+                                <span>
+                                    <i class="ri-settings-3-line"></i>
+                                </span>
+                                <img src={currentUser.photoURL} alt="" />
+                            </div>
+                        </div>
+                    </Container>
+                </div>
+            </header>
+
+            <section className="admin__menu p-0">
                 <Container>
-                    <div className="admin__nav-wrapper-top">
-                        <div className="logo">
-                            <h2>Multimart</h2>
+                    <Row>
+                        <div className="admin__navigation">
+                            <ul className="admin__menu-list">
+                                {admin_nav.map((item, index) => (
+                                    <li
+                                        className="admin__menu-item"
+                                        key={index}
+                                    >
+                                        <NavLink
+                                            to={item.path}
+                                            end
+                                            className={(navClass) =>
+                                                navClass.isActive
+                                                    ? "active__admin-menu"
+                                                    : ""
+                                            }
+                                        >
+                                            {item.display}
+                                        </NavLink>
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
-
-                        <div className="search__box">
-                            <input type="text" placeholder="Search..." />
-                            <span><i class="ri-search-line"></i></span>
-                        </div>
-
-                        <div className="admin__nav-top-right">
-                            <span><i class="ri-notification-2-line"></i></span>
-                            <span><i class="ri-settings-3-line"></i></span>
-                            <img src={currentUser.photoURL} alt="" />
-                        </div>
-                    </div>
+                    </Row>
                 </Container>
-            </div>
-        </header>
+            </section>
+        </>
     );
 };
 

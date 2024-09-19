6eeff6ae784f20b38c887c400cdd6ef1681639f1
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-19 15:42:30 +0700

    create getData hook

--- src/custom-hooks/useGetData.js
+++ src/custom-hooks/useGetData.js
@@ -1,9 +1,23 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { db } from "../firebase.config";
-import { collection, getDoc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 
+// Retrieves data from a specified Firestore collection.
+// This hook can be reused throughout a React application to fetch data from different Firestore collections dynamically.
 const useGetData = (collectionName) => {
-    return <div>useGetData</div>;
+    const [data, setData] = useState([]);
+    const collectionRef = collection(db, collectionName);
+
+    useEffect(() => {
+        const getData = async () => {
+            //Fetch all documents from the Firestore collection using getDocs(collectionRef).
+            const data = await getDocs(collectionRef);
+            setData(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        };
+
+        getData();
+    }, [collectionRef]);
+    return { data };
 };
 
 export default useGetData;

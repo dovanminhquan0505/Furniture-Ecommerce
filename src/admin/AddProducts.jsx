98339d7293faf44d418a822baf306a858296dcd3
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-19 15:04:57 +0700

    Create Products into Firebase

--- src/admin/AddProducts.jsx
+++ src/admin/AddProducts.jsx
@@ -1,13 +1,79 @@
-import React, {useState} from "react";
-import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Form, FormGroup, Card } from "reactstrap";
 import { motion } from "framer-motion";
+import { toast } from "react-toastify";
+import { db, storage } from "../firebase.config";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { collection, addDoc } from "firebase/firestore";
 
 const AddProducts = () => {
-    const [enterTitle, setEnterTitle] = useState('');
-    const [enterShortDesc, setEnterShortDesc] = useState('');
-    const [enterDescription, setEnterDescription] = useState('');
-    const [enterCategory, setEnterCategory] = useState('');
-    const [enterPrice, setEnterPrice] = useState('');
+    const [enterTitle, setEnterTitle] = useState("");
+    const [enterShortDesc, setEnterShortDesc] = useState("");
+    const [enterDescription, setEnterDescription] = useState("");
+    const [enterCategory, setEnterCategory] = useState("");
+    const [enterPrice, setEnterPrice] = useState("");
+    const [enterProductImg, setEnterProductImg] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    //Create Product
+    const createProduct = async (e) => {
+        e.preventDefault();
+
+        // const product = {
+        //     title: enterTitle,
+        //     shortDesc: enterShortDesc,
+        //     description: enterDescription,
+        //     category: enterCategory,
+        //     price: enterPrice,
+        //     imgURL: enterProductImg,
+        // };
+
+        //Create Product to the Firebase database
+        try {
+            const docRef = await collection(db, "products");
+
+            const storageRef = ref(
+                storage,
+                `productImages/${Date.now() + enterProductImg.name}`
+            );
+            const uploadTask = uploadBytesResumable(
+                storageRef,
+                enterProductImg
+            );
+
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const progress =
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log(`Upload is ${progress}% done`);
+                },
+                () => {
+                    toast.error("Images can not uploaded!");
+                    setLoading(false);
+                },
+                async () => {
+                    // After uploading finished, update file URL
+                    const downloadURL = await getDownloadURL(
+                        uploadTask.snapshot.ref
+                    );
+                    // Add a new document to Firestore with the URL of the uploaded image
+                    await addDoc(docRef, {
+                        title: enterTitle,
+                        shortDesc: enterShortDesc,
+                        description: enterDescription,
+                        category: enterCategory,
+                        price: enterPrice,
+                        imgURL: downloadURL,
+                    });
+                    toast.success("Product created successfully!");
+                    setLoading(false);
+                }
+            );
+        } catch (error) {}
+
+        // console.log(product);
+    };
 
     return (
         <section>
@@ -15,10 +81,18 @@ const AddProducts = () => {
                 <Row>
                     <Col lg="12">
                         <h4 className="mb-4">Create Product</h4>
-                        <Form>
+                        <Form onSubmit={createProduct}>
                             <FormGroup className="form__group">
                                 <span>Product title</span>
-                                <input type="text" placeholder="Double sofa" />
+                                <input
+                                    type="text"
+                                    placeholder="Double sofa"
+                                    value={enterTitle}
+                                    onChange={(e) =>
+                                        setEnterTitle(e.target.value)
+                                    }
+                                    required
+                                />
                             </FormGroup>
 
                             <FormGroup className="form__group">
@@ -26,6 +100,11 @@ const AddProducts = () => {
                                 <input
                                     type="text"
                                     placeholder="Sample short desc"
+                                    value={enterShortDesc}
+                                    onChange={(e) =>
+                                        setEnterShortDesc(e.target.value)
+                                    }
+                                    required
                                 />
                             </FormGroup>
 
@@ -34,18 +113,38 @@ const AddProducts = () => {
                                 <input
                                     type="text"
                                     placeholder="Description...."
+                                    value={enterDescription}
+                                    onChange={(e) =>
+                                        setEnterDescription(e.target.value)
+                                    }
+                                    required
                                 />
                             </FormGroup>
 
                             <div className="d-flex align-items-center">
                                 <FormGroup className="form__group">
                                     <span>Price</span>
-                                    <input type="number" placeholder="$100" />
+                                    <input
+                                        type="number"
+                                        placeholder="$100"
+                                        value={enterPrice}
+                                        onChange={(e) =>
+                                            setEnterPrice(e.target.value)
+                                        }
+                                        required
+                                    />
                                 </FormGroup>
 
                                 <FormGroup className="form__group">
                                     <span>Category</span>
-                                    <select className="p-2">
+                                    <select
+                                        className="p-2"
+                                        value={enterCategory}
+                                        onChange={(e) =>
+                                            setEnterCategory(e.target.value)
+                                        }
+                                        required
+                                    >
                                         <option value="chair">Chair</option>
                                         <option value="sofa">Sofa</option>
                                         <option value="mobile">Mobile</option>
@@ -60,11 +159,23 @@ const AddProducts = () => {
                             <div>
                                 <FormGroup className="form__group">
                                     <span>Product Image</span>
-                                    <input type="file" />
+                                    <input
+                                        type="file"
+                                        onChange={(e) =>
+                                            setEnterProductImg(
+                                                e.target.files[0]
+                                            )
+                                        }
+                                        required
+                                    />
                                 </FormGroup>
                             </div>
 
-                            <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
+                            <motion.button
+                                whileTap={{ scale: 1.2 }}
+                                className="buy__btn"
+                                type="submit"
+                            >
                                 Create Product
                             </motion.button>
                         </Form>

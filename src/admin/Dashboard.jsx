6f97766428618bbfa27190bf377a2facab9d5736
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-19 17:04:00 +0700

    Create Dashboard views

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../styles/dashboard.css";
+import useGetData from "../custom-hooks/useGetData";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    const {data: products} = useGetData('products');
+    const {data: users} = useGetData('users');
+
+    return (
+        <>
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg="3">
+                            <div className="orders__box">
+                                <h5>Orders</h5>
+                                <span>789</span>
+                            </div>
+                        </Col>
+                        <Col lg="3">
+                            <div className="products__box">
+                                <h5>Products</h5>
+                                <span>{products.length}</span>
+                            </div>
+                        </Col>
+                        <Col lg="3">
+                            <div className="users__box">
+                                <h5>Users</h5>
+                                <span>{users.length}</span>
+                            </div>
+                        </Col>
+                        <Col lg="3">
+                            <div className="revenue__box">
+                                <h5>Total Sales</h5>
+                                <span>$7890</span>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </>
+    );
 };
 
 export default Dashboard;

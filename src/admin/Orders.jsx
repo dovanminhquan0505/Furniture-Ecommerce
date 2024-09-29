75bf4ba016ee29ef017833597bf0f2333fd73f70
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-29 14:01:35 +0700

    Handle delete orders

--- src/admin/Orders.jsx
+++ src/admin/Orders.jsx
@@ -2,9 +2,22 @@ import React from "react";
 import { Container, Col, Row } from "reactstrap";
 import useGetData from "../custom-hooks/useGetData";
 import { motion } from "framer-motion";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import { doc, deleteDoc } from "firebase/firestore";
 
 const Orders = () => {
     const { data: ordersData, loading } = useGetData("orders");
+
+    // Handle delete orders
+    const deleteOrder = async (orderId) => {
+        try {
+            await deleteDoc(doc(db, "orders", orderId));
+            toast.success("Order deleted successfully!");
+        } catch (error) {
+            toast.error("Failed to delete order:" + error.message);
+        }
+    }
 
     return (
         <section>
@@ -30,7 +43,7 @@ const Orders = () => {
                                         Loading....
                                     </h6>
                                 ) : (
-                                    ordersData.map((order, index) => {
+                                    ordersData.map((order) => {
                                         const createdAt = order.createdAt
                                             ?.toDate()
                                             .toLocaleDateString("en-US");
@@ -46,7 +59,7 @@ const Orders = () => {
                                             : "No";
 
                                         return (
-                                            <tr key={index}>
+                                            <tr key={order.id}>
                                                 <td>
                                                     {order.paymentResult
                                                         ? order.paymentResult.id
@@ -73,6 +86,7 @@ const Orders = () => {
                                                             scale: 1.1,
                                                         }}
                                                         className="btn btn-danger"
+                                                        onClick={() => deleteOrder(order.id)}
                                                     >
                                                         Delete
                                                     </motion.button>

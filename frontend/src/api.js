d43cb1a4d8b3176f7b6fc00ad658e669080ec5fd
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2025-04-23 22:05:44 +0700

    Update API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 // Authentication && User
 export const registerUser = async (userData) => {
